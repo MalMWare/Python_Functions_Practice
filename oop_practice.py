b24a2406fd15d97fc3e0f513c397d41ec2b9e86f
MalMWare <malkelleymartin@gmail.com>
2023-05-25 15:15:51 -0700

    added class and methods

--- oop_practice.py
+++ oop_practice.py
@@ -4,6 +4,28 @@ class Race:
     self.car_limit = car_limit
     self.driver_limit = driver_limit
 
+  def add_driver(self, driver):
+    if len(self.car_limit) <= self.driver_limit:
+      self.car_limit.append(driver)
+
+  def get_average_ranking(self):
+    sum = 0
+    for d in self.car_limit:
+      sum += d.driver_ranking
+    return sum / len(self.car_limit)
+
+
+class Driver:
+  number_of_drivers = 0
+  def __init__(self, driver_name, driver_age, driver_ranking):
+    self.driver_name = driver_name
+    self.driver_age = driver_age
+    self.driver_ranking = driver_ranking
+    Driver.number_of_drivers += 1
+
+  def get_ranking(self):
+    return self.driver_ranking
+
 class Car:
   def __init__(self, name, max_speed):
     self.name = name
@@ -15,9 +37,23 @@ class Car:
   def talk(self, driver):
     print(f'Hello, {driver}, I am {self.name}.')
 
-race = Race("indy 500", [], 15)
+my_driver = Driver('Lewis Hamilton', 28, 1)
+print(my_driver.get_ranking())
 
-myCar = Car('Kitt', 180)
-myOtherCar = Car('Speedy', 55)
+d1 = Driver('Lewis Hamilton', 36, 83)
+d2 = Driver('Eliud Kipchoge', 36, 95)
+d3 = Driver('Sebastian Vettel', 34, 76)
+d4 = Driver('Ayrton Senna', 34, 99)
+race= Race("indy 500", [d1, d2, d3, d4], 15)
 
-myCar.talk('Michael')
+race.add_driver(d1)
+race.add_driver(d2)
+race.add_driver(d3)
+race.add_driver(d4)
+
+print(race.get_average_ranking())
+
+# myCar = Car('Kitt', 180)
+# myOtherCar = Car('Speedy', 55)
+
+# myCar.talk('Mal')
