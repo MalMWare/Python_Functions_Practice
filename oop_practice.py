619682ebc59849a156057d575c41b531715ad83e
MalMWare <malkelleymartin@gmail.com>
2023-05-24 23:10:20 -0700

    Race Class added

--- oop_practice.py
+++ oop_practice.py
@@ -1,3 +1,9 @@
+class Race: 
+  def __init__(self, race_name, car_limit, driver_limit):
+    self.race_name = race_name
+    self.car_limit = car_limit
+    self.driver_limit = driver_limit
+
 class Car:
   def __init__(self, name, max_speed):
     self.name = name
@@ -8,6 +14,8 @@ class Car:
 
   def talk(self, driver):
     print(f'Hello, {driver}, I am {self.name}.')
+
+race = Race("indy 500", [], 15)
 
 myCar = Car('Kitt', 180)
 myOtherCar = Car('Speedy', 55)
