51817c26b528d4f9e04fd35b3a15e30372191c59
MalMWare <malkelleymartin@gmail.com>
2023-05-25 20:48:06 -0700

    rewrite functions as lambda functions

--- lambda_practice.py
+++ lambda_practice.py
@@ -10,9 +10,16 @@ for i in range(10):
 
 
 my_dict = dict()
+names = ["Alice", "Bob", "Charlie"]
 for i in names:
-    my+dict[i] = i.upper()
+    my_dict[i] = i.upper()
 
 #Rewrite functions above as Lambda Functions
+perimeter = lambda side_length: side_length * 4
 
+alpha = "abcdefghijklmnopqrstuvwxyz"
+my_lst = [alpha[i] for i in range(10)]
+
+names = ["Alice", "Bob", "Charlie"]
+my_dict = {n: n.upper() for n in names}
 
