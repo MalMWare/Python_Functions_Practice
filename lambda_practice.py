6cd368db784fa7534c7820668230b30b365b6adc
MalMWare <malkelleymartin@gmail.com>
2023-05-25 19:22:47 -0700

    added lambda practice questions

---
+++ lambda_practice.py
@@ -0,0 +1,18 @@
+def perimeter(side_length):
+    return 4 * side_length
+
+
+my_lst = []
+alpha = "abcdefghijklmnopqrstuvwxyz"
+
+for i in range(10):
+    my_lst.append(alpha[i])
+
+
+my_dict = dict()
+for i in names:
+    my+dict[i] = i.upper()
+
+#Rewrite functions above as Lambda Functions
+
+
