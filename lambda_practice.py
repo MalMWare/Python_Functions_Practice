9487b1793266ef6bffdc299ed01e7a904ea4062a
MalMWare <malkelleymartin@gmail.com>
2023-05-28 22:25:44 -0700

    completed extra practice problems

--- lambda_practice.py
+++ lambda_practice.py
@@ -18,7 +18,8 @@ for i in names:
 # Problem 1: Write a lambda function to sort a list of tuples in ascending order according to the number in the second position of each tuple.
 
 grades = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
-
+sorted_grades = sorted(grades, key = lambda x: x[1])
+print(sorted_grades)
 # Problem 2: Write a lambda function to cube every element of a list
 #Original list: [3,6,9,2]
 
@@ -26,7 +27,8 @@ cubed = lambda x: [i**3 for i in x]
 print(cubed([3,6,9,2])) 
 
 #Problem 3: Write a lambda function to determine whether a number is even or odd (the function should return True or False), and then use the function and a list comprehension to create a new list of booleans, where even numbers are True and odd numbers are False.
-
+even_odd = lambda x: True if x%2 == 0 else False
+print([even_odd(x) for x in [3,6,9,2]])
 
 # Problem 4: Use a list comprehension to create a list of the numbers from 1 to 100 (including 100).
 #Rewrite functions above as Lambda Functions
@@ -34,6 +36,9 @@ num = [i for i in range(1, 101)]
 print(num)
 
 # Problem 5: Use a dictionary comprehension to count the length of each word in a sentence.
+x = "The quick brown fox jumped over the fence."
+print({w:len(w) for w in x.split()})
+
 perimeter = lambda side_length: side_length * 4
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
